8d16b6a6f1b4530288b26c2609911052aa52ac0f
Kevin <kevinjss97@gmail.com>
2022-11-24 11:59:43 +0100

    ADD: New service ReadFile, class Word, new resource word.txt, new page test MOD: Navmenu add route to test and singleton for ReadFileService

---
+++ Data/ReadFileService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace WordleSolver.Data
+{
+    public sealed class ReadFileService
+    {
+        private static List<Word> wordList = null;
+        public ReadFileService()
+        {
+        }
+
+        public static async Task<List<Word>> GetWordsListAsync(string path)
+        {
+
+            if(wordList == null)
+            {
+            using var stream = await FileSystem.OpenAppPackageFileAsync(path);
+            using var reader = new StreamReader(stream);
+            var data = await reader.ReadToEndAsync();
+            wordList = data.Split("\n").Select(i=>new Word
+            {
+                StringWord= i,
+            }).ToList();
+                return wordList;
+            }
+            else
+            {
+                return wordList;
+            }
+        }
+    }
+}
---
+++ Data/Word.cs
@@ -0,0 +1,8 @@
+﻿namespace WordleSolver.Data
+{
+    public class Word
+    {
+        public string StringWord { get; set; }
+
+    }
+}
