5424c1e2af576b66c85eca890272aa32be97de38
Aiuola <angelo.bonicelli@gmail.com>
2022-11-24 23:29:48 +0100

    first try at a decent looking ui

--- Data/ReadFileService.cs
+++ Data/ReadFileService.cs
@@ -1,31 +1,33 @@
 ﻿using System.Diagnostics;
-using System.IO;
 
 namespace WordleSolver.Data
 {
-    public static class ReadFileService
-    {
-        private static List<string> wordList = null;
-        public static async Task<List<string>> GetWordsListAsync(string path)
-        {
-            if (wordList != null)
-            {
-                Debug.WriteLine("File already loaded");
-                return wordList;
-            }
+	public static class ReadFileService
+	{
+		private static List<string> wordList = null;
+		public static async Task<List<string>> GetWordsListAsync(string path)
+		{
+			if (wordList != null)
+			{
+				Debug.WriteLine("File already loaded");
+				return wordList;
+			}
 
-            try
-            {
-                using var stream = await FileSystem.OpenAppPackageFileAsync(path);
-                using var reader = new StreamReader(stream);
-                var data = await reader.ReadToEndAsync();
-                wordList = data.Split("\n").Select(i => i).ToList();
-                return wordList;
-            }
-            catch (FileNotFoundException ex) {
-                Debug.WriteLine(ex);
-                throw ex;
-            }
-        }
-    }
+			try
+			{
+				using var stream = await FileSystem.OpenAppPackageFileAsync(path);
+				using var reader = new StreamReader(stream);
+				var data = await reader.ReadToEndAsync();
+				wordList = data.Split("\n").Select(i => i).ToList();
+
+				Debug.WriteLine("File read successfully");
+				return wordList;
+			}
+			catch (FileNotFoundException ex)
+			{
+				Debug.WriteLine(ex);
+				throw ex;
+			}
+		}
+	}
 }
