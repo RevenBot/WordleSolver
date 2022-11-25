9cca6ee10de2f1f459748634a4f29e5ec77221fe
Aiuola <angelo.bonicelli@gmail.com>
2022-11-25 12:20:06 +0100

    fix: re-added the interface

--- Data/ReadFileService.cs
+++ Data/ReadFileService.cs
@@ -4,13 +4,13 @@ namespace WordleSolver.Data
 {
 	public static class ReadFileService
 	{
-		private static List<string> wordList = null;
-		public static async Task<List<string>> GetWordsListAsync(string path)
+		private static List<IWord> wordsList = null;
+		public static async Task<List<IWord>> GetWordsListAsync(string path)
 		{
-			if (wordList != null)
+			if (wordsList != null)
 			{
 				Debug.WriteLine("File already loaded");
-				return wordList;
+				return wordsList;
 			}
 
 			try
@@ -18,10 +18,13 @@ namespace WordleSolver.Data
 				using var stream = await FileSystem.OpenAppPackageFileAsync(path);
 				using var reader = new StreamReader(stream);
 				var data = await reader.ReadToEndAsync();
-				wordList = data.Split("\n").Select(i => i).ToList();
+				wordsList = new List<IWord>();
+				foreach (string s in data.Split("\n")) {
+					wordsList.Add(new Word(s)); 
+				}
 
 				Debug.WriteLine("File read successfully");
-				return wordList;
+				return wordsList;
 			}
 			catch (FileNotFoundException ex)
 			{
---
+++ Data/Word.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleSolver.Data
+{
+    public class Word : IWord
+    {
+        public Word(string word) {
+            this.Value = word;
+        }
+        public string Value { get; set; }
+    }
+}
